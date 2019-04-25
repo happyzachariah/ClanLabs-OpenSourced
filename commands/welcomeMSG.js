7cd12703239a24e5d271b1aaa7aa639196824b79
Nishant Srivastava <nishi7409@gmail.com>
2019-04-25 15:03:05 -0400

    update ok

---
+++ commands/welcomeMSG.js
@@ -0,0 +1,10 @@
+/* this is assuming your group is private (request to join)*/
+module.exports = {
+	name: 'view',
+	description: '!view [username1]',
+	execute(message, args) {
+
+    //stuff
+
+  },
+}
